b9072e092fb63c7054db18a655dd5b81f9594d04
Clinton Francis <clfranci@microsoft.com>
2017-05-08 11:58:00 +0200

    Gradients now are able to use their own frames for positioning in multi gradients

--- src/Styles.Color.Shared/Gradients/Gradient.cs
+++ src/Styles.Color.Shared/Gradients/Gradient.cs
@@ -3,208 +3,240 @@ using System.Drawing;
 
 namespace Styles.Color
 {
-	// CSS Gradients
-	// https://www.w3schools.com/css/css3_gradients.asp
+    // CSS Gradients
+    // https://www.w3schools.com/css/css3_gradients.asp
 
-	public enum GradientType
-	{
-		None,
-		Linear,
-		Radial,
-		Multi,
-		Custom
-	}
+    public enum GradientType
+    {
+        None,
+        Linear,
+        Radial,
+        Multi,
+        Custom
+    }
 
-	public class ColorOffset
-	{
-		public IRgb Color { get; set;}
-		public double Offset { get; set;}
+    public class ColorOffset
+    {
+        public IRgb Color { get; set; }
+        public double Offset { get; set; }
 
-		public ColorOffset(IRgb color, double offset)
-		{
-			Color = color;
-			Offset = offset;
-		}	
-	}
+        public ColorOffset(IRgb color, double offset)
+        {
+            Color = color;
+            Offset = offset;
+        }
+    }
 
-	public class Gradient
-	{
-		public GradientType Type { get; protected set;}
+    public class Gradient
+    {
+        // TODO Implement in Bait & Switch
+        public RectangleF Frame { get; set; }
 
-		public IRgb[] Colors { get; set; }
+        public GradientType Type { get; protected set; }
 
-		public float[] Locations { get; set; } = new float[0];
+        public IRgb[] Colors { get; set; }
 
-		// TODO Reversed
-		public bool Reversed { get; set; }
+        public float[] Locations { get; set; } = new float[0];
 
-		// TODO repeating
-		public bool Repeating { get; set; }	
+        // TODO Reversed
+        public bool Reversed { get; set; }
 
-		// TODO ScaleMultipliers
-		public PointF ScaleMultiplier { get; set; } = new PointF(1f, 1f);
+        // TODO repeating
+        public bool Repeating { get; set; }
 
-		public ColorOffset[] Offsets 
-		{
-			get{
-				var offsets = new ColorOffset[Colors.Length];
-				for (int i = 0; i< Colors.Length; i++)
-				{
-					offsets[i] = new ColorOffset(Colors[i], Locations[i]);
-				}
-				return offsets;
-			}
-		}
+        // TODO ScaleMultipliers
+        public PointF ScaleMultiplier { get; set; } = new PointF(1f, 1f);
 
-		public Gradient(IRgb[] colors)
-		{
-			this.Colors = colors;
-		}
+        public ColorOffset[] Offsets
+        {
+            get
+            {
+                var offsets = new ColorOffset[Colors.Length];
+                for (int i = 0; i < Colors.Length; i++)
+                {
+                    offsets[i] = new ColorOffset(Colors[i], Locations[i]);
+                }
+                return offsets;
+            }
+        }
 
-		public void Update()
-		{
-			// todo check for colors == 2 as well
-			if (Colors.Length < 2)
-			{
-				throw new Exception("Gradients must contain a minimum of two colors");
-			}
+        public Gradient(IRgb[] colors)
+        {
+            this.Colors = colors;
+        }
 
-			if (Locations.Length == 0)
-			{
-				float stepSize = 1f / (Colors.Length - 1f);
-				float currentStep = 0;
-				Locations = new float[Colors.Length];
+        // Work around method while not working in bait and switch
+        public void SetFrame(float x, float y, float width, float height)
+        {
+            this.Frame = new RectangleF(x, y, width, height);
+        }
 
-				for (int i = 0; i < Colors.Length; i++)
-				{
-					Locations[i] = currentStep;
-					currentStep += stepSize;
-				}
-			} else if (Locations.Length != Colors.Length)
-			{
-				throw new Exception("Gradient Colors length is not equal to its Locations length");
-			}
-		}
+        public void Update()
+        {
+            if (Colors.Length < 2)
+            {
+                throw new Exception("Gradients must contain a minimum of two colors");
+            }
 
-		public static Gradient FromColors<T>(IColorSpace[] colors)
-		{
-			throw new NotImplementedException();
-		}
+            if (Locations.Length == 0)
+            {
+                float stepSize = 1f / (Colors.Length - 1f);
+                float currentStep = 0;
+                Locations = new float[Colors.Length];
 
-		public LinearGradient ToLinear(double orientation = 0)
-		{
-			return new LinearGradient(this.Colors, orientation) { Locations = this.Locations };
-		}
+                for (int i = 0; i < Colors.Length; i++)
+                {
+                    Locations[i] = currentStep;
+                    currentStep += stepSize;
+                }
+            }
+            else if (Locations.Length != Colors.Length)
+            {
+                throw new Exception("Gradient Colors length is not equal to its Locations length");
+            }
+        }
 
-		public RadialGradient ToRadial(float locationX = .5f, float locationY = .5f)
-		{
-			return new RadialGradient(this.Colors, locationX, locationY){ Locations = this.Locations };
-		}
+        public static Gradient FromColors<T>(IColorSpace[] colors)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Gradient ShiftHues(double value)
-		{
-			var target = this.Clone();
-			for (int i = 0; i < target.Colors.Length; i++)
-			{
-				target.Colors[i] = target.Colors[i].AdjustHue(value);
-			}
+        public LinearGradient ToLinear(double orientation = 0)
+        {
+            return new LinearGradient(this.Colors, orientation)
+            {
+                Locations = this.Locations,
+                Frame = this.Frame,
+                Reversed = this.Reversed,
+                Repeating = this.Repeating,
+                ScaleMultiplier = this.ScaleMultiplier
+            };
+        }
 
-			return target;
-		}
+        public RadialGradient ToRadial(float locationX = .5f, float locationY = .5f)
+        {
+            return new RadialGradient(this.Colors, locationX, locationY)
+            {
+                Locations = this.Locations,
+                Frame = this.Frame,
+                Reversed = this.Reversed,
+                Repeating = this.Repeating,
+                ScaleMultiplier = this.ScaleMultiplier
+            };
+        }
 
-		/// <summary>
-		/// Returns the color at the given scale by interpolating the colors.
-		/// </summary>
-		/// <returns>IRgb<see cref="T:Styles.IRgb"/>.</returns>
-		/// <param name="percent">Percent</param>
-		public IRgb GetColorAt(double percent){
-			return GradientUtils.GetColorByOffset(this, percent);
-		}
+        public Gradient ShiftHues(double value)
+        {
+            var target = this.Clone();
+            for (int i = 0; i < target.Colors.Length; i++)
+            {
+                target.Colors[i] = target.Colors[i].AdjustHue(value);
+            }
 
-		/// <summary>
-		/// Returns a color IRgb palette of x steps by selecting equidistant colors.
-		/// </summary>
-		/// <returns>Array of IRgb colors</returns>
-		/// <param name="steps">The number of color steps to return. 2 by default</param>
-		public IRgb[] CreateColorPalette(int steps = 2)
-		{
-			var stepSize = 1.0 / steps;
-			var colorPalette = new IRgb[steps];
-			for (int i = 0; i < steps; i++)
-			{
-				var step = stepSize * i;
-				colorPalette[i] = GradientUtils.GetColorByOffset(this, step);
-			}
+            return target;
+        }
 
-			return colorPalette;
-		}
+        /// <summary>
+        /// Returns the color at the given scale by interpolating the colors.
+        /// </summary>
+        /// <returns>IRgb<see cref="T:Styles.IRgb"/>.</returns>
+        /// <param name="percent">Percent</param>
+        public IRgb GetColorAt(double percent)
+        {
+            return GradientUtils.GetColorByOffset(this, percent);
+        }
 
-		public Gradient Clone()
-		{
-			return new Gradient((IRgb[])Colors.Clone())
-			{
-				Locations = this.Locations,
-				Reversed = this.Reversed,
-				Repeating = this.Repeating,
-				ScaleMultiplier = this.ScaleMultiplier
-			};
-		}
-	}
+        /// <summary>
+        /// Returns a color IRgb palette of x steps by selecting equidistant colors.
+        /// </summary>
+        /// <returns>Array of IRgb colors</returns>
+        /// <param name="steps">The number of color steps to return. 2 by default</param>
+        public IRgb[] CreateColorPalette(int steps = 2)
+        {
+            var stepSize = 1.0 / steps;
+            var colorPalette = new IRgb[steps];
+            for (int i = 0; i < steps; i++)
+            {
+                var step = stepSize * i;
+                colorPalette[i] = GradientUtils.GetColorByOffset(this, step);
+            }
 
-	public class LinearGradient : Gradient
-	{
-		// -360 -> 0 <- 360
-		public double Rotation { get; set; }
+            return colorPalette;
+        }
 
-		public LinearGradient(IRgb[] colors, double rotation):base(colors)
-		{
-			Rotation = rotation;
-			Type = GradientType.Linear;
-		}
+        public Gradient Clone()
+        {
+            return new Gradient((IRgb[])Colors.Clone())
+            {
+                Locations = this.Locations,
+                Reversed = this.Reversed,
+                Repeating = this.Repeating,
+                ScaleMultiplier = this.ScaleMultiplier,
+                Frame = this.Frame
+            };
+        }
+    }
 
-		new public LinearGradient Clone()
-		{
-			return new LinearGradient((IRgb[])Colors.Clone(), this.Rotation)
-			{
-				Locations = this.Locations,
-				Reversed = this.Reversed,
-				Repeating = this.Repeating,
-				ScaleMultiplier = this.ScaleMultiplier
-			};
-		}
-	}
+    public class LinearGradient : Gradient
+    {
+        // -360 -> 0 <- 360
+        public double Rotation { get; set; }
 
-	public class RadialGradient:Gradient
-	{
-		public PointF Location { get; set; }
+        public LinearGradient(IRgb[] colors, double rotation) : base(colors)
+        {
+            Rotation = rotation;
+            Type = GradientType.Linear;
+        }
 
-		public RadialGradient(IRgb[] colors):base(colors)
-		{
-			Location = new PointF(0, 0);
-			Type = GradientType.Radial;
-		}
+        new public LinearGradient Clone()
+        {
+            return new LinearGradient((IRgb[])Colors.Clone(), this.Rotation)
+            {
+                Locations = this.Locations,
+                Reversed = this.Reversed,
+                Repeating = this.Repeating,
+                ScaleMultiplier = this.ScaleMultiplier
+            };
+        }
+    }
 
-		public RadialGradient(IRgb[] colors, float locationX, float locationY):base(colors)
-		{
-			Location = new PointF(locationX, locationY);
-			Type = GradientType.Radial;
-		}
+    public class RadialGradient : Gradient
+    {
+        // TODO Implement in Bait & Switch
+        public PointF Location { get; set; }
 
-		new public RadialGradient Clone()
-		{
-			return new RadialGradient((IRgb[])Colors.Clone(), this.Location.X, this.Location.Y)
-			{
-				Locations = this.Locations,
-				Reversed = this.Reversed,
-				Repeating = this.Repeating,
-				ScaleMultiplier = this.ScaleMultiplier
-			};
-		}
-	}
+        public RadialGradient(IRgb[] colors) : base(colors)
+        {
+            Location = new PointF(0, 0);
+            Type = GradientType.Radial;
+        }
 
-	public class MultiGradient
-	{
-		public Gradient[] Gradients { get; set; } // Do I need to bother with this? could I just Set this up as an extension to Gradient[]'s?
-		// todo consider how offset points, radius, etc would work with these - do i need to extend the base classes?
-	}
+        public RadialGradient(IRgb[] colors, float locationX, float locationY) : base(colors)
+        {
+            Location = new PointF(locationX, locationY);
+            Type = GradientType.Radial;
+        }
+
+        public RadialGradient(IRgb[] colors, PointF location) : base(colors)
+        {
+            Location = location;
+            Type = GradientType.Radial;
+        }
+
+        new public RadialGradient Clone()
+        {
+            return new RadialGradient((IRgb[])Colors.Clone(), this.Location.X, this.Location.Y)
+            {
+                Locations = this.Locations,
+                Reversed = this.Reversed,
+                Repeating = this.Repeating,
+                ScaleMultiplier = this.ScaleMultiplier
+            };
+        }
+    }
+
+    public class MultiGradient
+    {
+        public Gradient[] Gradients { get; set; } // Do I need to bother with this? could I just Set this up as an extension to Gradient[]'s?
+    }
 }
--- src/Styles.Color.iOS/Gradients/GradientExtensions.cs
+++ src/Styles.Color.iOS/Gradients/GradientExtensions.cs
@@ -6,163 +6,196 @@ using Foundation;
 
 namespace Styles.Color
 {
-	
-	public static class GradientExtensions
-	{
-		#region Helper methods
-		static double[] DetermineGradientPoints(LinearGradient target)
-		{
-			// create coordinates
-			var x = target.Rotation / 360d;
-			var a = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.75) / 2))), 2);
-			var b = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.0) / 2))), 2);
-			var c = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.25) / 2))), 2);
-			var d = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.5) / 2))), 2);
 
-			return new double[] { a, b, c, d };
-		}
+    public static class GradientExtensions
+    {
+        #region Helper methods
+        static double[] DetermineGradientPoints(LinearGradient target)
+        {
+            // create coordinates
+            var x = target.Rotation / 360d;
+            var a = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.75) / 2))), 2);
+            var b = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.0) / 2))), 2);
+            var c = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.25) / 2))), 2);
+            var d = Math.Pow(Math.Sin((2 * Math.PI * ((x + 0.5) / 2))), 2);
 
-		static nfloat[] ConvertToNativeArray(float[] target)
-		{
-			var converted = new nfloat[target.Length];
-			for (int i = 0; i < target.Length; i++)
-			{
-				converted[i] = target[i];
-			}
-			return converted;
-		}
+            return new double[] { a, b, c, d };
+        }
 
-		static nfloat[] GenerateGradientColors(IRgb[] colors)
-		{
-			var totalColors = colors.Length * 4;
-			nfloat[] gradColors = new nfloat[totalColors];
+        static nfloat[] ConvertToNativeArray(float[] target)
+        {
+            var converted = new nfloat[target.Length];
+            for (int i = 0; i < target.Length; i++)
+            {
+                converted[i] = target[i];
+            }
+            return converted;
+        }
 
-			var offset = 0;
-			for (int i = 0; i < colors.Length; i++)
-			{
-				gradColors[offset] = (nfloat)colors[i].R;
-				gradColors[offset + 1] = (nfloat)colors[i].G;
-				gradColors[offset + 2] = (nfloat)colors[i].B;
-				gradColors[offset + 3] = (nfloat)colors[i].A;
+        static nfloat[] GenerateGradientColors(IRgb[] colors)
+        {
+            var totalColors = colors.Length * 4;
+            nfloat[] gradColors = new nfloat[totalColors];
 
-				offset += 4;
-			}
+            var offset = 0;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                gradColors[offset] = (nfloat)colors[i].R;
+                gradColors[offset + 1] = (nfloat)colors[i].G;
+                gradColors[offset + 2] = (nfloat)colors[i].B;
+                gradColors[offset + 3] = (nfloat)colors[i].A;
 
-			return gradColors;
-		}
-		#endregion
+                offset += 4;
+            }
 
-		#region View Extensions
-		public static GradientView ToNativeView(this LinearGradient target, CGRect frame)
-		{
-			return new GradientView(target) { Frame = frame };
-		}
+            return gradColors;
+        }
+        #endregion
 
-		public static GradientView ToNativeView(this RadialGradient target, CGRect frame)
-		{
-			return new GradientView(target) { Frame = frame };
-		}
+        #region View Extensions
+        public static GradientView ToNativeView(this LinearGradient target, CGRect frame)
+        {
+            return new GradientView(target) { Frame = frame };
+        }
 
-		public static GradientView ToNativeView(this MultiGradient target, CGRect frame)
-		{
-			return new GradientView(target) { Frame = frame };
-		}
+        public static GradientView ToNativeView(this RadialGradient target, CGRect frame)
+        {
+            return new GradientView(target) { Frame = frame };
+        }
 
-		public static GradientView ToNativeView(this Gradient[] targets, CGRect frame)
-		{
-			throw new NotImplementedException();
-		}
+        public static GradientView ToNativeView(this MultiGradient target, CGRect frame)
+        {
+            return new GradientView(target) { Frame = frame };
+        }
 
-		public static CAGradientLayer ToNativeLayer(this LinearGradient target, CGRect frame)
-		{
-			target.Update();
+        public static GradientView ToNativeView(this Gradient[] targets, CGRect frame)
+        {
 
-			var gradLayer = new CAGradientLayer();
-			gradLayer.Frame = frame;
+            return new GradientView(targets) { Frame = frame };
+        }
 
-			var colors = new CGColor[target.Colors.Length];
-			for (int i = 0; i < target.Colors.Length; i++)
-			{
-				var rgb = (ColorRGB)target.Colors[i];
-				colors[i] = rgb.ToNative().CGColor;
-			}
+        public static CAGradientLayer ToNativeLayer(this LinearGradient target, CGRect frame)
+        {
+            target.Update();
 
-			gradLayer.Colors = colors;
+            var gradLayer = new CAGradientLayer();
+            gradLayer.Frame = frame;
 
-			var locations = new NSNumber[target.Locations.Length];
-			for (int i = 0; i < target.Locations.Length; i++)
-			{
-				locations[i] = target.Locations[i];
-			}
+            var colors = new CGColor[target.Colors.Length];
+            for (int i = 0; i < target.Colors.Length; i++)
+            {
+                var rgb = (ColorRGB)target.Colors[i];
+                colors[i] = rgb.ToNative().CGColor;
+            }
 
-			gradLayer.Locations = locations;
+            gradLayer.Colors = colors;
 
-			var points = DetermineGradientPoints(target);
-			gradLayer.StartPoint = new CGPoint(points[0], points[1]);
-			gradLayer.EndPoint = new CGPoint(points[2], points[3]);
+            var locations = new NSNumber[target.Locations.Length];
+            for (int i = 0; i < target.Locations.Length; i++)
+            {
+                locations[i] = target.Locations[i];
+            }
 
-			return gradLayer;
-		}
-		#endregion
+            gradLayer.Locations = locations;
 
-		#region Draw Extensions
-		public static void Draw(this LinearGradient target, CGContext ctx, CGRect bounds)
-		{
-			target.Update();
+            var points = DetermineGradientPoints(target);
+            gradLayer.StartPoint = new CGPoint(points[0], points[1]);
+            gradLayer.EndPoint = new CGPoint(points[2], points[3]);
 
-			var gradColors = GenerateGradientColors(target.Colors);
-			var colorSpace = CGColorSpace.CreateDeviceRGB();
-			var grad = new CGGradient(colorSpace, gradColors, ConvertToNativeArray(target.Locations) );
+            return gradLayer;
+        }
+        #endregion
 
-			var points = DetermineGradientPoints(target);
-			var startPoint = new CGPoint(points[0] * bounds.Width, points[1] * bounds.Height);
-			var endPoint = new CGPoint(points[2] * bounds.Width, points[3] * bounds.Height);
+        #region Draw Extensions
+        public static void Draw(this LinearGradient target, CGContext ctx, CGRect bounds)
+        {
+            target.Update();
 
-			ctx.DrawLinearGradient(grad, startPoint, endPoint, CGGradientDrawingOptions.DrawsBeforeStartLocation | CGGradientDrawingOptions.DrawsAfterEndLocation);
-			grad.Dispose();
-			colorSpace.Dispose();
-		}
+            CGRect clippingBounds = bounds;
+            if (!target.Frame.IsEmpty)
+                bounds = new CGRect(target.Frame.X, target.Frame.Y, target.Frame.Width, target.Frame.Height);
 
-		public static void Draw(this RadialGradient target, CGContext ctx, CGRect bounds)
-		{
-			target.Update();
+            var gradColors = GenerateGradientColors(target.Colors);
+            var colorSpace = CGColorSpace.CreateDeviceRGB();
+            var grad = new CGGradient(colorSpace, gradColors, ConvertToNativeArray(target.Locations));
 
-			var gradColors = GenerateGradientColors(target.Colors);
-			var colorSpace = CGColorSpace.CreateDeviceRGB();
-			var grad = new CGGradient(colorSpace, gradColors, ConvertToNativeArray(target.Locations));
-			var gradCenter = new CGPoint(bounds.Width * target.Location.X, bounds.Height * target.Location.Y);
-			var gradRadius = (float)Math.Min(bounds.Size.Width * target.ScaleMultiplier.X, bounds.Size.Height * target.ScaleMultiplier.Y);
+            var points = DetermineGradientPoints(target);
+            var startPoint = new CGPoint(points[0] * bounds.Width, points[1] * bounds.Height);
+            var endPoint = new CGPoint(points[2] * bounds.Width, points[3] * bounds.Height);
 
-			ctx.DrawRadialGradient(grad, gradCenter, 0, gradCenter, gradRadius, CGGradientDrawingOptions.DrawsBeforeStartLocation | CGGradientDrawingOptions.DrawsAfterEndLocation);
-			grad.Dispose();
-			colorSpace.Dispose();
-		}
+            // Crop to frame
+            if (!target.Frame.IsEmpty)
+            {
+                ctx.SaveState();
+                ctx.ClipToRect(clippingBounds);
+                ctx.DrawLinearGradient(grad, startPoint, endPoint, CGGradientDrawingOptions.DrawsBeforeStartLocation | CGGradientDrawingOptions.DrawsAfterEndLocation);
+                ctx.RestoreState();
+            }
+            else
+            {
+                ctx.DrawLinearGradient(grad, startPoint, endPoint, CGGradientDrawingOptions.DrawsBeforeStartLocation | CGGradientDrawingOptions.DrawsAfterEndLocation);
+            }
 
-		public static void Draw(this Gradient[] items, CGContext ctx, CGRect bounds)
-		{
-			throw new NotImplementedException();
-		}
+            grad.Dispose();
+            colorSpace.Dispose();
+        }
 
-		public static void Draw(this MultiGradient target, CGContext ctx, CGRect bounds)
-		{
-			for (int i = 0; i < target.Gradients.Length; i++)
-			{
-				var gradient = target.Gradients[i];
-				switch (gradient.Type)
-				{
-					case GradientType.Linear:
-						var linearGradient = gradient as LinearGradient;
-						linearGradient.Draw(ctx, bounds);
-						break;
-					case GradientType.Radial:
-						var radialGradient = gradient as RadialGradient;
-						radialGradient.Draw(ctx, bounds);
-						break;
-					default:
-						throw new NotImplementedException("Multigradients does not support the supplied gradient type: " + gradient.Type);
-				}
-			}
-		}
-		#endregion
-	}
+        public static void Draw(this RadialGradient target, CGContext ctx, CGRect bounds)
+        {
+            target.Update();
+
+            CGRect clippingBounds = bounds;
+            if (!target.Frame.IsEmpty)
+                bounds = new CGRect(target.Frame.X, target.Frame.Y, target.Frame.Width, target.Frame.Height);
+
+            var gradColors = GenerateGradientColors(target.Colors);
+            var colorSpace = CGColorSpace.CreateDeviceRGB();
+            var grad = new CGGradient(colorSpace, gradColors, ConvertToNativeArray(target.Locations));
+            var gradCenter = new CGPoint(bounds.Width * target.Location.X, bounds.Height * target.Location.Y);
+            var gradRadius = (float)Math.Min(bounds.Size.Width * target.ScaleMultiplier.X, bounds.Size.Height * target.ScaleMultiplier.Y);
+
+            // Crop to frame
+            if (!target.Frame.IsEmpty)
+            {
+                ctx.SaveState();
+                ctx.ClipToRect(clippingBounds);
+                ctx.DrawRadialGradient(grad, gradCenter, 0, gradCenter, gradRadius, CGGradientDrawingOptions.DrawsBeforeStartLocation | CGGradientDrawingOptions.DrawsAfterEndLocation);
+                ctx.RestoreState();
+            }
+            else
+            {
+                ctx.DrawRadialGradient(grad, gradCenter, 0, gradCenter, gradRadius, CGGradientDrawingOptions.DrawsBeforeStartLocation | CGGradientDrawingOptions.DrawsAfterEndLocation);
+            }
+
+            grad.Dispose();
+            colorSpace.Dispose();
+        }
+
+        public static void Draw(this Gradient[] items, CGContext ctx, CGRect bounds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Draw(this MultiGradient target, CGContext ctx, CGRect bounds)
+        {
+            for (int i = 0; i < target.Gradients.Length; i++)
+            {
+                var gradient = target.Gradients[i];
+                switch (gradient.Type)
+                {
+                    case GradientType.Linear:
+                        var linearGradient = gradient as LinearGradient;
+                        linearGradient.Draw(ctx, bounds);
+                        break;
+                    case GradientType.Radial:
+                        var radialGradient = gradient as RadialGradient;
+                        radialGradient.Draw(ctx, bounds);
+                        break;
+                    default:
+                        throw new NotImplementedException("Multigradients does not support the supplied gradient type: " + gradient.Type);
+                }
+            }
+        }
+        #endregion
+    }
 }
